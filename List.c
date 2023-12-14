ca84d9ae39e1e922e9fc39c80014ad4f6a1cad08
Nacali <nakaly@gmail.com>
2023-12-14 16:09:01 +0000

    Add swap_nodes and sort_List methods

--- List.c
+++ List.c
@@ -102,3 +102,66 @@ int delete_element(List_t self, unsigned int i) {
 unsigned int size(List_t self) {
 	return self->n_elements;
 }
+
+int swap_nodes(List_t self, unsigned int i, unsigned int j) {
+	Node_t *current_i, *current_j, *previous_i, *previous_j, *aux;
+	unsigned int index_i, index_j;
+	
+	if ( i == j || i > self->n-elements || j > self->n-elements) {
+		printf("Wrong input\n");
+		return -1;
+	}
+	
+	current_i = self->inner_list;
+	for (index_i = 0; index_i < i; index_i++) {
+		if (current_i == NULL) {
+			printf("Wrong input\n");
+			return -1;
+		}
+		previous_i = current_i;
+		current_i = current_i->next_node;
+	}
+	
+	current_j = self->inner_list;
+	for (index_j = 0; index_j < j; index_j++) {
+		if (current_j == NULL) {
+			printf("Wrong input\n");
+			return -1;
+		}
+		previous_j = current_j;
+		current_j = current_j->next_node;
+	}
+	
+	previous_i->next_node = current_j;
+	previous_j->next_node = current_i;
+	aux = current_j->next_node;
+	current_j->next_node = current_i->next_node;
+	current_i->next_node = aux;
+	return 0;
+}
+
+int sort_List(List_t self) {
+	unsigned int i, j;
+	
+	if (self->n_elements < 2) {
+		printf("Not enough elements to sort\n");
+		return -1;
+	}
+	current_i = self->inner_list;
+	for (i = 0; i < self->n_elements; i++) {
+		current_j = current_i->next_node;
+		for (j = i + 1; j < self->n_elements; j++) {
+			if (current_i == NULL || current_j == NULL) {
+				printf("Wrong input\n");
+				return -2;
+			}
+			else {
+				if (current_j->element < current_j->element) {
+					swap_nodes(self, i, j);
+				}
+			}
+			current_j = current_j->next_node;
+		}
+		current_i = current_i->next_node;
+	}
+}
